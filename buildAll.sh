24eb8cc64d858080c69e773584b4e9a489a2cdc1
StéphaneDucasse <stephane.ducasse@inria.fr>
2021-09-26 16:52:01 +0200

    first version not fully working but UsingFigures is working.

---
+++ buildAll.sh
@@ -0,0 +1,12 @@
+mkdir __results
+
+for OUTPUT in $(find Slides -name '*.pillar')
+do
+	x=$OUTPUT
+	echo $x
+	pillar build pdf $OUTPUT
+	echo _result/pdf/${x%.pillar}.pdf
+	cp _result/pdf/${x%.pillar}.pdf __results
+done
+
+
