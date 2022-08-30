558ccf7512ea004b7ee6458813b6134b69b707cf
StéphaneDucasse <stephane.ducasse@inria.fr>
2022-08-30 16:22:13 +0200

    rm luc; starting checking resources

--- buildAll.sh
+++ buildAll.sh
@@ -10,3 +10,11 @@ do
 done
 
 
+for OUTPUT in $(find Slides/Lectures -name '*.pillar')
+do
+	x=$OUTPUT
+	echo $x
+	pillar build pdf $OUTPUT
+	echo _result/pdf/${x%.pillar}.pdf
+	cp _result/pdf/${x%.pillar}.pdf __results
+done
