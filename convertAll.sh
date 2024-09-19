7da5cb8e5eb858fe2f4792be20126753ed1b96b7
StéphaneDucasse <stephane.ducasse@inria.fr>
2024-09-19 10:09:27 +0200

    rescue conversion script + pluggin in the pillar conf!

---
+++ convertAll.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+%rm -fr __results
+%mkdir __results
+
+./toMDConf.sh
+
+buildSlide() {
+	f=$(dirname $1)/$(basename $1 .pillar)
+	echo $f
+	pillar convertAlternateSlide ${f%}.pillar
+	pillar build pdf ${f%}.md
+	echo _result/pdf/${f%}.pdf
+	cp _result/pdf/${f%}.pdf __results
+}
+
+for PILLAR_FILE in $(find Slides/Syntax -name '*.pillar')
+do
+	buildSlide $PILLAR_FILE
+done
+
+# for PILLAR_FILE in $(find Slides/Lectures -name '*.pillar')
+# do
+# 	buildSlide $PILLAR_FILE
+# done
